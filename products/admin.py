f7104019fa779d5e0db4c439e6bd055ff67bc7b2
a2sh3r <ashurmetovmm@gmail.com>
2023-12-20 03:36:31 +0300

    -added Cake model -added Trifle model -added Cupcake model -added Marshmallow model -added Ginger model -added description field in Product model -added ingredients field in Product model

--- products/admin.py
+++ products/admin.py
@@ -1,5 +1,15 @@
 from django.contrib import admin
 
-from products.models import Product
+from products.models import Product, Cake, Trifle, Marshmallow, Ginger, Cupcake
 
 admin.site.register(Product)
+
+admin.site.register(Cake)
+
+admin.site.register(Trifle)
+
+admin.site.register(Marshmallow)
+
+admin.site.register(Ginger)
+
+admin.site.register(Cupcake)
