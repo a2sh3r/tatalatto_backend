1d95939cec769510a10efcc2a27848179592558b
a2sh3r <ashurmetovmm@gmail.com>
2023-12-20 03:44:30 +0300

    -added TODO's for further development

--- products/tests.py
+++ products/tests.py
@@ -1,3 +1,4 @@
 from django.test import TestCase
 
 # Create your tests here.
+#TODO try to use popular modules for testing and try a test-driven dev approach
--- products/views.py
+++ products/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+#TODO create views for every Product's inherited models only for querrying
