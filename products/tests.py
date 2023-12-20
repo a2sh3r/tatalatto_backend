681e44af3d8c84ae8e3e8289f0e6f4205a7f4df9
a2sh3r <ashurmetovmm@gmail.com>
2023-12-20 16:13:07 +0300

    -added tests for creating and representing Cake model

--- products/tests.py
+++ products/tests.py
@@ -1,4 +1,23 @@
 from django.test import TestCase
 
-# Create your tests here.
-#TODO try to use popular modules for testing and try a test-driven dev approach
+from products.models import Cake
+
+
+class CakeModelTests(TestCase):
+    """Тест модели Cake"""
+
+    def setUp(self):
+        self.cake = Cake(
+            product_name='test-case-cake',
+            price='2999',
+            product_description='9.99',
+            product_ingredients='Test text for ingredients',
+            cake_topping='[cherry, chocolate, biscuit]',
+            product_type='CAKE',
+        )
+
+    def test_create_book(self):
+        self.assertIsInstance(self.cake, Cake)
+
+    def test_str_representation(self):
+        self.assertEquals(str(self.cake), "test-case-cake")
